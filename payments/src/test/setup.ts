bf3e2896acc6b3c7a93444b1c02d9caa8243dd98
Damin <daminmutti@icloud.com>
2024-05-19 01:49:01 -0700

    Fix structuredClone issue in payments tests

--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -1,21 +1,27 @@
-import { MongoMemoryServer } from 'mongodb-memory-server';
-import mongoose from 'mongoose';
-import request from 'supertest';
-import { app } from '../app';
-import jwt from 'jsonwebtoken';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+global.structuredClone =
+  global.structuredClone || require("core-js-pure/features/structured-clone");
+
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import request from "supertest";
+import { app } from "../app";
+import jwt from "jsonwebtoken";
 
 declare global {
   var signin: (id?: string) => string[];
 }
 
-jest.mock('../nats-wrapper');
+jest.mock("../nats-wrapper");
 
-process.env.STRIPE_KEY = 'sk_test_hnfrAm8rOkryFEnV23jjfFlw';
+process.env.STRIPE_KEY = "sk_test_hnfrAm8rOkryFEnV23jjfFlw";
 
 let mongo: any;
 beforeAll(async () => {
-  process.env.JWT_KEY = 'asdfasdf';
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+  process.env.JWT_KEY = "asdfasdf";
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
   const mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
@@ -43,7 +49,7 @@ global.signin = (id?: string) => {
   // Build a JWT payload.  { id, email }
   const payload = {
     id: id || new mongoose.Types.ObjectId().toHexString(),
-    email: 'test@test.com',
+    email: "test@test.com",
   };
 
   // Create the JWT!
@@ -56,7 +62,7 @@ global.signin = (id?: string) => {
   const sessionJSON = JSON.stringify(session);
 
   // Take JSON and encode it as base64
-  const base64 = Buffer.from(sessionJSON).toString('base64');
+  const base64 = Buffer.from(sessionJSON).toString("base64");
 
   // return a string thats the cookie with the encoded data
   return [`session=${base64}`];
