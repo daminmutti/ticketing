1ac54328cb7a2bfc6ce8907c1ac8691a4eafe5ab
Damin <daminmutti@icloud.com>
2024-06-16 03:59:57 -0700

    fixed url to URL in client directory

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export default ({ req }) => {
   if (typeof window === "undefined") {
     // We are on the server
-
     return axios.create({
       baseURL: "http://www.ticketing-app-prod.ca/",
       headers: req.headers,
@@ -11,7 +10,7 @@ export default ({ req }) => {
   } else {
     // We must be on the browser
     return axios.create({
-      baseUrl: "/",
+      baseURL: "/",
     });
   }
 };
