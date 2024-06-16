7e3db80d961d984ad5259e7dc06570d256812fbc
Damin <daminmutti@icloud.com>
2024-06-16 04:34:59 -0700

    changed slash

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,7 @@ export default ({ req }) => {
   if (typeof window === "undefined") {
     // We are on the server
     return axios.create({
-      baseURL: "http://www.ticketing-app-prod.ca/",
+      baseURL: "http://www.ticketing-app-prod.ca",
       headers: req.headers,
     });
   } else {
